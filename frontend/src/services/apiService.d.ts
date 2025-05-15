82b3b299d099e8a9cae13fd0fa31de14c967717a
7dys3 <151423414+7dys3@users.noreply.github.com>
2025-05-15 12:36:39 +0800

    Delete frontend directory

--- frontend/src/services/apiService.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "@/services/apiService" {
-  const apiService: {
-    get: <T = any>(url: string, config?: any) => Promise<T>;
-    post: <T = any>(url: string, data?: any, config?: any) => Promise<T>;
-    put: <T = any>(url: string, data?: any, config?: any) => Promise<T>;
-    delete: <T = any>(url: string, config?: any) => Promise<T>;
-  };
-  export { apiService };
-}
